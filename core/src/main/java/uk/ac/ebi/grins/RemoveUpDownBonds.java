9df08d642a35808f2e190912785786f1f47ef266
John May <john.wilkinsonmay@gmail.com>
2013-08-09 14:41:03 +0100

    Removal of redundant up/down bonds - needs more work to be correct.

---
+++ core/src/main/java/uk/ac/ebi/grins/RemoveUpDownBonds.java
@@ -0,0 +1,203 @@
+/*
+ * Copyright (c) 2013, European Bioinformatics Institute (EMBL-EBI)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are those
+ * of the authors and should not be interpreted as representing official policies,
+ * either expressed or implied, of the FreeBSD Project.
+ */
+
+package uk.ac.ebi.grins;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * Given a molecule with explict double bond configurations remove redundant
+ * UP/DOWN bond. For example the removing redundant up/down labels from of
+ * {@code N/C(/C)=C\C} produces {@code N/C(C)=C\C}.
+ *
+ * @author John May
+ */
+final class RemoveUpDownBonds {
+
+    public ChemicalGraph transform(final ChemicalGraph g)
+            throws InvalidSmilesException {
+
+        ChemicalGraph h = new ChemicalGraph(g.order());
+
+        // copy atom/topology information this is unchanged
+        for (int u = 0; u < g.order(); u++) {
+            h.addAtom(g.atom(u));
+            h.addTopology(g.topologyOf(u));
+        }
+
+        int[] ordering = new DepthFirstOrder(g).visited;
+
+        Map<Edge, Edge> replacements = new HashMap<Edge, Edge>();
+
+        // change edges (only changed added to replacement)
+        for (int u = 0; u < g.order(); u++) {
+            for (final Edge e : g.edges(u)) {
+                if (e.other(u) > u && e.bond() == Bond.DOUBLE) {
+                    removeRedundant(g, e, ordering, replacements);
+                }
+            }
+        }
+
+        // append the edges, replacing any which need to be changed
+        for (int u = 0; u < g.order(); u++) {
+            for (Edge e : g.edges(u)) {
+                if (e.other(u) > u) {
+                    Edge replacement = replacements.get(e);
+                    if (replacement != null)
+                        e = replacement;
+                    h.addEdge(e);
+                }
+            }
+        }
+
+        return h;
+    }
+
+    /**
+     * Given a double bond edge traverse the neighbors of both endpoints and
+     * accumulate any explicit replacements in the 'acc' accumulator.
+     *
+     * @param g   the chemical graph
+     * @param e   a edge in the graph ('double bond type')
+     * @param acc accumulator for new edges
+     * @throws uk.ac.ebi.grins.InvalidSmilesException
+     *          thrown if the edge could not be converted
+     */
+    private void removeRedundant(ChemicalGraph g,
+                                 Edge e,
+                                 int[] ordering,
+                                 Map<Edge, Edge> acc)
+            throws InvalidSmilesException {
+
+        int u = e.either(), v = e.other(u);
+
+        replaceImplWithExpl(g, e, u, ordering, acc);
+        replaceImplWithExpl(g, e, v, ordering, acc);
+    }
+
+    /**
+     * Given a double bond edge traverse the neighbors of one of the endpoints
+     * and accumulate any explicit replacements in the 'acc' accumulator.
+     *
+     * @param g   the chemical graph
+     * @param e   a edge in the graph ('double bond type')
+     * @param u   a endpoint of the edge 'e'
+     * @param acc accumulator for new edges
+     * @throws uk.ac.ebi.grins.InvalidSmilesException
+     *          thrown if the edge could not be converted
+     */
+    private void replaceImplWithExpl(final ChemicalGraph g,
+                                     final Edge e,
+                                     final int u,
+                                     final int[] ordering,
+                                     final Map<Edge, Edge> acc)
+            throws InvalidSmilesException {
+
+        Set<Edge> edges = new TreeSet<Edge>(Collections
+                                                    .reverseOrder(new Comparator<Edge>() {
+                                                        @Override
+                                                        public int compare(Edge e, Edge f) {
+                                                            int v = ordering[e
+                                                                    .other(u)];
+                                                            int w = ordering[f
+                                                                    .other(u)];
+                                                            if (v > w)
+                                                                return +1;
+                                                            if (v < w)
+                                                                return -1;
+                                                            return 0;
+                                                        }
+                                                    }));
+
+        for (Edge f : g.edges(u)) {
+            switch (f.bond()) {
+                case DOUBLE:
+                    if (!f.equals(e))
+                        return;
+                    break;
+                case UP:
+                case DOWN:
+                    edges.add(f);
+                    break;
+            }
+        }
+
+        if (edges.size() > 1) {
+            Iterator<Edge> it = edges.iterator();
+            Edge explicit = it.next();
+
+            // Ensure we don't complete remove all stereo bonds in conjugated
+            // system. If the 'higher' order edge has already been replaced
+            // don't make the other edge implicit
+            if (!acc.containsKey(it.next())) {
+
+                int v = explicit.either();
+                int w = explicit.other(v);
+                acc.put(explicit, new Edge(v,
+                                           w,
+                                           Bond.IMPLICIT));
+            }
+
+        }
+    }
+
+    private static final class DepthFirstOrder {
+        private final ChemicalGraph g;
+        private final int[]         visited;
+        private       int           i;
+
+        private DepthFirstOrder(ChemicalGraph g) {
+            this.g = g;
+            this.visited = new int[g.order()];
+            Arrays.fill(visited, -1);
+            for (int u = 0; u < g.order(); u++) {
+                if (visited[u] < 0) {
+                    visit(u);
+                }
+            }
+        }
+
+        private void visit(int u) {
+            visited[u] = i++;
+            for (Edge e : g.edges(u)) {
+                int v = e.other(u);
+                if (visited[v] < 0) {
+                    visit(v);
+                }
+            }
+        }
+    }
+}
---
+++ core/src/test/java/uk/ac/ebi/grins/RemoveUpDownBondsTest.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2013, European Bioinformatics Institute (EMBL-EBI)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are those
+ * of the authors and should not be interpreted as representing official policies,
+ * either expressed or implied, of the FreeBSD Project.
+ */
+
+package uk.ac.ebi.grins;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+
+/** @author John May */
+public class RemoveUpDownBondsTest {
+
+    @Test public void e_butene_expH() throws Exception {
+        transform("C\\C(\\[H])=C\\C",
+                  "C\\C([H])=C\\C");
+        transform("C/C(/[H])=C/C",
+                  "C/C([H])=C/C");
+        transform("C\\C(\\[H])=C(/[H])\\C",
+                  "C\\C([H])=C(/[H])C");
+        transform("C/C(/[H])=C(\\[H])/C",
+                  "C/C([H])=C(\\[H])C");
+    }
+
+    @Test public void z_butene_expH() throws Exception {
+        transform("C\\C(\\[H])=C/C",
+                  "C\\C([H])=C/C");
+        transform("C/C(/[H])=C\\C",
+                  "C/C([H])=C\\C");
+        transform("C\\C(\\[H])=C(\\[H])/C",
+                  "C\\C([H])=C(\\[H])C");
+        transform("C/C(/[H])=C(/[H])\\C",
+                  "C/C([H])=C(/[H])C");
+    }
+
+    @Test public void e_e_hexadiene_expH() throws InvalidSmilesException {
+        transform("C\\C(\\[H])=C(/[H])\\C(\\[H])=C(/[H])\\C",
+                  "C\\C([H])=C(/[H])C(\\[H])=C(/[H])C");
+    }
+
+
+    static void transform(String smi, String exp) throws
+                                                  InvalidSmilesException {
+        Assert.assertThat(Generator.generate(new RemoveUpDownBonds()
+                                                     .transform(Parser.parse(smi))),
+                          is(exp));
+    }
+}
