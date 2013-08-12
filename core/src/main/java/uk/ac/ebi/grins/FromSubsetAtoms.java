7a1c355885ea58d3d3f9f554422fe35399733213
John May <john.wilkinsonmay@gmail.com>
2013-08-12 11:28:05 +0100

    Conversion from a graph with subset atoms to one without subset atoms. Every atom is 'bracket' atom with a set hydrogen count etc.

---
+++ core/src/main/java/uk/ac/ebi/grins/FromSubsetAtoms.java
@@ -0,0 +1,53 @@
+package uk.ac.ebi.grins;
+
+import java.util.List;
+
+/**
+ * Given a chemical graph with 0 or more atoms. Convert that graph to one where
+ * all atoms are fully specified bracket atoms.
+ *
+ * @author John May
+ */
+final class FromSubsetAtoms {
+
+    ChemicalGraph transform(ChemicalGraph g) {
+
+        ChemicalGraph h = new ChemicalGraph(g.order());
+
+        for (int u = 0; u < g.order(); u++) {
+            h.addAtom(fromSubset(g.atom(u),
+                                 bondOrderSum(g.edges(u))));
+            h.addTopology(g.topologyOf(u));
+        }
+
+        // edges are unchanged
+        for (Edge e : g.edges())
+            h.addEdge(e);
+
+        return h;
+    }
+
+    static Atom fromSubset(Atom a, int bondOrderSum) {
+
+        // atom is already a non-subset atom
+        if (!a.subset())
+            return a;
+
+        int hCount = a.element().implicitHydrogens(bondOrderSum);
+
+        return new Atom.BracketAtom(-1,
+                                    a.element(),
+                                    hCount,
+                                    0,
+                                    0,
+                                    a.aromatic());
+    }
+
+    private int bondOrderSum(final List<Edge> es) {
+        int nElectrons = 0;
+        for (Edge e : es)
+            nElectrons += e.bond().electrons();
+        return nElectrons / 2;
+    }
+
+}
---
+++ core/src/test/java/uk/ac/ebi/grins/FromSubsetAtomsTest.java
@@ -0,0 +1,72 @@
+package uk.ac.ebi.grins;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.sameInstance;
+import static org.junit.Assert.assertThat;
+
+/** @author John May */
+public class FromSubsetAtomsTest {
+
+    @Test public void inorganic() throws Exception {
+        transform("[*]", "[*]");
+    }
+
+    @Test public void inorganic2() throws Exception {
+        transform("[Ne]", "[Ne]");
+    }
+
+    @Test public void methane() throws Exception {
+        transform("C", "[CH4]");
+    }
+
+    @Test public void ethane_withAtomClass() throws Exception {
+        transform("[CH3:1]C", "[CH3:1][CH3]");
+    }
+
+    @Test public void ethanol() throws InvalidSmilesException {
+        transform("CCO", "[CH3][CH2][OH]");
+    }
+
+    @Test public void stereoSpecification() throws InvalidSmilesException {
+        transform("[C@H](N)(O)C", "[C@H]([NH2])([OH])[CH3]");
+        transform("[C@@H](N)(O)C", "[C@@H]([NH2])([OH])[CH3]");
+    }
+
+    @Test public void noStereoSpecification() throws InvalidSmilesException {
+        transform("C(N)(O)C", "[CH]([NH2])([OH])[CH3]");
+    }
+
+    @Test public void bracketAtom() {
+        // should provide identity of bracket atom
+        Atom input  = new Atom.BracketAtom(Element.Carbon, 1, 0);
+        Atom output = FromSubsetAtoms.fromSubset(input, 0);
+        assertThat(input, is(sameInstance(output)));
+    }
+
+    @Test public void aliphatic_carbon() {
+        Atom actual = FromSubsetAtoms.fromSubset(Atom.AliphaticSubset.Carbon, 3);
+        Atom expect = new Atom.BracketAtom(Element.Carbon, 1, 0);
+        assertThat(expect, is(actual));
+    }
+
+    @Test public void aromatic_carbon() {
+        Atom actual = FromSubsetAtoms.fromSubset(Atom.AromaticSubset.Carbon, 3);
+        Atom expect = new Atom.BracketAtom(-1, Element.Carbon, 1, 0, 0, true);
+        assertThat(expect, is(actual));
+    }
+
+    private void transform(String input, String expected) throws
+                                                          InvalidSmilesException {
+        ChemicalGraph g = Parser.parse(input);
+        ImplicitToExplicit ite = new ImplicitToExplicit();
+        FromSubsetAtoms    fsa = new FromSubsetAtoms();
+        ExplicitToImplicit eti = new ExplicitToImplicit();
+        String actual = Generator.generate(eti.transform(
+                                           fsa.transform(
+                                           ite.transform(g))));
+        assertThat(actual, is(expected));
+    }
+
+}
