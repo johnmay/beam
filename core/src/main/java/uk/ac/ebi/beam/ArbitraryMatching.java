4b33126e120e251d9f73f540f549661448d5ab83
John May <john.wilkinsonmay@gmail.com>
2013-09-02 18:13:42 +0100

    Producing an arbitrary matching on a subset of vertices in a graph.

---
+++ core/src/main/java/uk/ac/ebi/beam/ArbitraryMatching.java
@@ -0,0 +1,45 @@
+package uk.ac.ebi.beam;
+
+/**
+ * Simple matching greedily chooses edges and matches. The produced matching is
+ * not guaranteed to be maximum but provides a starting point for improvement
+ * through augmentation.
+ *
+ * @author John May
+ */
+final class ArbitraryMatching {
+
+    /**
+     * Create an arbitrary matching on the subset of vertices ('s') of provided
+     * graph.
+     *
+     * @param g graph to match
+     * @param s subset of vertices
+     * @return non-maximal matching
+     */
+    static Matching of(final Graph g, final boolean[] s) {
+
+        final Matching m = Matching.empty(g);
+
+        for (int v = 0; v < g.order(); v++) {
+
+            // skip if not in the subset of vertices or already matched
+            if (!s[v] || m.contains(v))
+                continue;
+
+            // find a single edge which is not matched and match it
+            for (Edge e : g.edges(v)) {
+                int w = e.other(v);
+
+                if (!s[w] || m.contains(w))
+                    continue;
+
+                m.match(v, w);
+                break;
+            }
+        }
+
+        return m;
+    }
+
+}
