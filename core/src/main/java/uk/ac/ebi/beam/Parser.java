6fa8af72bfbb493c4a1cbfd6eac8e291722be40c
John May <john.wilkinsonmay@gmail.com>
2013-10-03 16:48:42 +0100

    Double the provided rnum not the current size of the ring storage.

--- core/src/main/java/uk/ac/ebi/beam/Parser.java
+++ core/src/main/java/uk/ac/ebi/beam/Parser.java
@@ -586,7 +586,7 @@ final class Parser {
     private void openRing(int rnum) {
         if (rnum >= rings.length)
             rings = Arrays.copyOf(rings,
-                                  Math.min(99, rings.length * 2)); // max rnum: 99
+                                  Math.min(99, rnum * 2)); // max rnum: 99
         int u = stack.peek();
 
         // create a ring bond
