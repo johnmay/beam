a2c34451f24b7a7472abf731204baa0f2f7f9504
John May <john.wilkinsonmay@gmail.com>
2013-12-10 14:04:01 +0000

    Intercept unknown atoms and set as non-aromatic.

--- core/src/main/java/uk/ac/ebi/beam/AtomBuilder.java
+++ core/src/main/java/uk/ac/ebi/beam/AtomBuilder.java
@@ -118,8 +118,10 @@ public final class AtomBuilder {
     public static AtomBuilder aromatic(Element e) {
         if (e == null)
             throw new NullPointerException("no element provided");
+        if (e == Element.Unknown)
+            return new AtomBuilder(e, false);
         if (!e.aromatic(Element.AromaticSpecification.Daylight))
-            throw new IllegalArgumentException(e + " cannot be aromatic by the Daylight specification.");
+            throw new IllegalArgumentException("The element '" + e + "' cannot be aromatic by the Daylight specification.");
         return new AtomBuilder(e, true);
     }
 
